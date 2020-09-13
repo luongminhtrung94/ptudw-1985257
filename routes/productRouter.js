cab9f3ada66a7012063ae237cb51ce55c2059b15
luongminhtrung94 <luongminhtrung1691994@gmail.com>
2020-09-13 21:43:54 +0700

    update products

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,7 +3,24 @@ let router = express.Router();
 
 
 router.get("/", (req,res)=>{
-    res.render("category");
+    let categoryController = require("../controllers/categoryController");
+    categoryController.getAll().then(data =>{
+        res.locals.categories = data;
+        let brandController = require("../controllers/brandController");
+        return brandController.getAll();
+    }).then(data =>{
+        res.locals.brands = data;
+        let colorController = require("../controllers/colorController");
+        return colorController.getAll();
+    }).then(data =>{
+        res.locals.colors = data;
+        let productController = require("../controllers/productController");
+        return productController.getAll();
+    }).then(data =>{
+        res.locals.products = data;
+        console.log(data);
+        res.render("category");
+    });
 });
 
 router.get("/:id", (req,res)=>{
